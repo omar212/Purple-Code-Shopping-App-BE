3877d05b712d8187197257f40ff197faedaef28f
Omar Elnagdy <omar@macbookpro.mynetworksettings.com>
2025-02-25 14:16:20 -0500

    added prisma for user data login functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequest) {}
+  constructor(private readonly prismaService: PrismaService) {}
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          email: true,
+          id: true,
+        },
+      });
+    } catch (error: unknown) {
+      console.log(error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new UnprocessableEntityException('User already exists');
+        }
+      }
+      throw error;
+    }
+  }
 }
